3200ed6628e0111fefefcfe19d214ae3bca8d408
JubairFaruque <161877703+JubairFaruque@users.noreply.github.com>
2024-03-02 00:45:45 +0600

    Add files via upload

---
+++ single_profile.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+
+<?php
+include 'connection.php';
+?>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>CSM OFFICE MANAGEMENT</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: NiceAdmin
+  * Updated: Nov 17 2023 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header fixed-top d-flex align-items-center">
+
+    <div class="d-flex align-items-center justify-content-between">
+      <a href="index.html" class="logo d-flex align-items-center">
+        <img src="assets/img/logo.png" alt="">
+        <span class="d-none d-lg-block">CSM</span>
+      </a>
+      <i class="bi bi-list toggle-sidebar-btn"></i>
+    </div><!-- End Logo -->
+
+    <div class="search-bar">
+	  <center><p><h1>Office Management</h1></p></center>
+    </div><!-- End Search Bar -->
+
+    <nav class="header-nav ms-auto">
+      <ul class="d-flex align-items-center">
+
+        <li class="nav-item d-block d-lg-none">
+          <a class="nav-link nav-icon search-bar-toggle " href="#">
+            <i class="bi bi-search"></i>
+          </a>
+        </li><!-- End Search Icon-->
+
+        <!--<li class="nav-item dropdown">
+		
+          </ul><!-- End Notification Dropdown Items -->
+
+        </li><!-- End Notification Nav -->
+
+        <!--<li class="nav-item dropdown">
+		
+            </li>-->
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+
+            <!--<li class="dropdown-footer">
+              <a href="#">Show all messages</a>
+            </li>
+
+          </ul><!-- End Messages Dropdown Items -->
+
+        </li><!-- End Messages Nav -->
+
+        <!--<li class="nav-item dropdown pe-3">
+        </li><!-- End Profile Nav -->
+
+      </ul>
+    </nav><!-- End Icons Navigation -->
+
+  </header><!-- End Header -->
+
+  <!-- ======= Sidebar ======= -->
+  <aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+      <li class="nav-heading">Pages</li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+          <i class="bi bi-menu-button-wide"></i><span>Profile</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+          <li>
+            <a href="account_display.php">
+              <i class="bi bi-circle"></i><span>See Profiles</span>
+            </a>
+          </li>
+          <li>
+            <a href="account.php">
+              <i class="bi bi-circle"></i><span>Create Profile</span>
+            </a>
+          </li>
+        </ul>
+      </li><!-- End Profile Page Nav -->
+
+      <!--<li class="nav-item">
+      </li><!-- End Contact Page Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="register.php">
+          <i class="bi bi-card-list"></i>
+          <span>Register</span>
+        </a>
+      </li><!-- End Register Page Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="login.php">
+          <i class="bi bi-box-arrow-in-right"></i>
+          <span>Login</span>
+        </a>
+      </li><!-- End Login Page Nav -->
+	  <li class="nav-item">
+        <a class="nav-link collapsed" href="pages-login.html">
+          <i class="bi bi-box-arrow-in-right"></i>
+          <span>Notice Box</span>
+        </a>
+      </li><!--
+      <!--<li class="nav-item">
+        
+      </li><!-- End Error 404 Page Nav -->
+
+      <!--<li class="nav-item">
+     
+      </li><!-- End Blank Page Nav -->
+
+    </ul>
+
+  </aside><!-- End Sidebar-->
+
+ <main id="main" class="main">
+
+    <div class="pagetitle">
+      
+        <!--<br><br><br><br><br><br><br><br><br><center><p align="center"><h1><b style="color: red">Welcome To</b><br>Department of Computer Science and Mathematics<br>Faculty of Agricultural Engineering And Technology <br> Bangladesh Agricultural University, Mymensingh</h1></p></center><br><br><br><br><br><br><br><br>-->
+		<table class="table">
+			<thead>
+				<tr>
+					<th scope="col" style="color: black;">Office ID</th>
+					<th scope="col" style="color: black;">Name</th>
+					<th scope="col" style="color: black;">Designation</th>
+					<th scope="col" style="color: black;">Mobile</th>
+					<th scope="col" style="color: black;">E-mail</th>
+					<th scope="col" style="color: black;">Present Address</th>
+					<th scope="col" style="color: black;">Joining Date</th>
+				</tr>
+			</thead>
+			<tbody>
+	
+            <?php
+			
+			$sql="Select * from profile where Name LIKE '%$name%'" ;
+			$result=mysqli_query($con,$sql);
+			if ($result)
+			{
+				while($row=mysqli_fetch_array($result)){
+					$id=$row['Office_ID'];
+					$name=$row['Name'];
+					$designation=$row['Designation'];
+					$mobile=$row['Mobile'];
+					$mail=$row['Email'];
+					$address=$row['Address'];
+					$date=$row['Joining_Date'];
+					echo '<tr>
+					<th scope="row" style="color: black;">'.$id.'</th>
+					<td style="color: black;">'.$name.'</td>
+					<td style="color: black;">'.$designation.'</td>
+					<td style="color: black;">'.$mobile.'</td>
+					<td style="color: black;">'.$mail.'</td>
+					<td style="color: black;">'.$address.'</td>
+					<td style="color: black;">'.$date.'</td>
+					<td style="color: black;">
+					<button class="btn btn-primary" name="Update"><a href="Update_donor_info (2).php?updateDonor_ID='.$id.'" class="text-light">Update</a></button>
+					<button class="btn btn-danger my-2" name="Delete"><a href="delete_donor_info.php?deleteDonor_ID='.$id.'" class="text-light">Delete</a></button>
+					</td>
+				</tr>';
+				}
+			}
+			
+			?>
+				
+			</tbody>
+		</table>
+
+	</div>
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+    <div class="copyright">
+      &copy; Copyright <strong><span>Jubair and Fawzia</span></strong>. All Rights Reserved
+    </div>
+    <!--<div class="credits">
+      <!-- All the links in the footer should remain intact. -->
+      <!-- You can delete the links only if you purchased the pro version. -->
+      <!-- Licensing information: https://bootstrapmade.com/license/ -->
+      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ 
+      Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+    </div> -->
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/chart.js/chart.umd.js"></script>
+  <script src="assets/vendor/echarts/echarts.min.js"></script>
+  <script src="assets/vendor/quill/quill.min.js"></script>
+  <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
